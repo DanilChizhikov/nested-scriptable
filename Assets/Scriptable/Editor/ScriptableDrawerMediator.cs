13dd78d702eac9b6affc3db3d720e49acd848dd9
Danil Chizhikov <danilchizhikov@MBP-Danil.cn.ru_novotelecom.ru>
2024-02-18 23:12:16 +0700

    remove draw footer and add draw dropdown

--- Assets/Scriptable/Editor/ScriptableDrawerMediator.cs
+++ Assets/Scriptable/Editor/ScriptableDrawerMediator.cs
@@ -45,17 +45,11 @@ namespace MBSCore.Scriptable
 
 	internal sealed class ScriptableDrawerMediator<T> : ScriptableDrawerMediator where T : ScriptableObject
 	{
-		private const string CreateFieldName = "Create Type";
 		private const string ElementTemplate = "Element {0}";
 		private const float ObjectWeight = 1f;
 		private const float NameWeight = 1f;
 		private const float SumWeight = ObjectWeight + NameWeight;
 		private const float ColumnSpace = 6f;
-
-		private readonly Dictionary<T, Editor> _editors;
-
-		private int _lastCreateIndex = -1;
-		private int _lastChangedIndex = -1;
 
 		public ScriptableDrawerMediator(string label, FieldInfo fieldInfo, ReorderableList reorderableList,
 			ScriptableObject scriptableObject) : base(label, fieldInfo, reorderableList, scriptableObject)
@@ -63,74 +57,9 @@ namespace MBSCore.Scriptable
 			reorderableList.drawElementCallback += DrawElement;
 			reorderableList.drawHeaderCallback += DrawHeader;
 			reorderableList.elementHeightCallback += CalculateElementHeight;
-			reorderableList.onChangedCallback += OnChangedCallback;
-			reorderableList.drawFooterCallback += DrawFooterCallback;
-			_editors = new Dictionary<T, Editor>();
+			reorderableList.onAddDropdownCallback += ShowDropdownElements;
+			reorderableList.onRemoveCallback += RemoveElement;
 		}
-
-		private void OnChangedCallback(ReorderableList list)
-		{
-			_lastChangedIndex = list.index;
-		}
-
-		private void AddElement()
-		{
-			string createdTypeName = ScriptableTypeController<T>.Names[_lastCreateIndex];
-			_lastCreateIndex = -1;
-			T elementConfig = ScriptableTypeController<T>.CreateInstance(createdTypeName);
-			elementConfig.name = createdTypeName;
-			if (List.list is Array array)
-			{
-				var newArray = new T[array.Length + 1];
-				array.CopyTo(newArray, 0);
-				FieldInfo.SetValue(Target, newArray);
-				List.list = newArray;
-			}
-			else
-			{
-				List.list.Add(null);
-			}
-			
-			List.list[^1] = elementConfig;
-			AssetDatabase.AddObjectToAsset(elementConfig, Target);
-			AssetDatabase.SaveAssets();
-		}
-
-		private float CalculateElementHeight(int index)
-		{
-			float height = EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
-			return height;
-		}
-		
-		private void DrawFooterCallback(Rect rect)
-		{
-			if (_lastCreateIndex > ScriptableTypeController<T>.Names.Length)
-			{
-				_lastCreateIndex = -1;
-			}
-
-			float buttonWidth = Mathf.Min((rect.width - 30f) / 2f, 50f);
-			float popupWidth = rect.width - 30f - buttonWidth * 2f;
-			var selectPopupRect = new Rect(rect.x, rect.y, popupWidth, EditorGUIUtility.singleLineHeight);
-			_lastCreateIndex = EditorGUI.Popup(selectPopupRect, CreateFieldName, _lastCreateIndex, ScriptableTypeController<T>.Names);
-			var addButtonRect = new Rect(rect.x + popupWidth + 10f, rect.y, buttonWidth, EditorGUIUtility.singleLineHeight);
-			if (GUI.Button(addButtonRect, "+") && CanAdd())
-			{
-				AddElement();
-			}
-
-			var removeButtonRect = new Rect(rect.x + popupWidth + 20f + buttonWidth, rect.y, buttonWidth, EditorGUIUtility.singleLineHeight);
-			if (GUI.Button(removeButtonRect, "-"))
-			{
-				RemoveElement();
-			}
-		}
-
-		private bool CanAdd() =>
-			_lastCreateIndex >= 0 && _lastCreateIndex <ScriptableTypeController<T>.Names.Length;
-
-		private void DrawHeader(Rect rect) =>
-			EditorGUI.LabelField(rect, Label);
 
 		private void DrawElement(Rect rect, int index, bool isActive, bool isFocused)
 		{
@@ -140,6 +69,47 @@ namespace MBSCore.Scriptable
 			{
 				DrawNameField(rect, weightWidth, value, usedWidth);
 			}
+		}
+		
+		private void DrawHeader(Rect rect) =>
+			EditorGUI.LabelField(rect, Label);
+		
+		private float CalculateElementHeight(int index)
+		{
+			float height = EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+			return height;
+		}
+		
+		private void ShowDropdownElements(Rect rect, ReorderableList list)
+		{
+			var dropdownMenu = new GenericMenu();
+			string[] names = ScriptableTypeController<T>.Names;
+			for (int i = 0; i < names.Length; i++)
+			{
+				string createdTypeName = names[i];
+				dropdownMenu.AddItem(new GUIContent(createdTypeName), false, () =>
+				{
+					T elementConfig = ScriptableTypeController<T>.CreateInstance(createdTypeName);
+					elementConfig.name = createdTypeName;
+					if (List.list is Array array)
+					{
+						var newArray = new T[array.Length + 1];
+						array.CopyTo(newArray, 0);
+						FieldInfo.SetValue(Target, newArray);
+						List.list = newArray;
+					}
+					else
+					{
+						List.list.Add(null);
+					}
+			
+					List.list[^1] = elementConfig;
+					AssetDatabase.AddObjectToAsset(elementConfig, Target);
+					AssetDatabase.SaveAssets();
+				});
+			}
+			
+			dropdownMenu.ShowAsContext();
 		}
 
 		private T DrawObjectField(Rect rect, float weightWidth, int index, out float usedWidth)
@@ -164,7 +134,7 @@ namespace MBSCore.Scriptable
 			return value;
 		}
 
-		private static void DrawNameField(Rect rect, float weightWidth, T value, float usedWidth)
+		private void DrawNameField(Rect rect, float weightWidth, T value, float usedWidth)
 		{
 			var nameRect = new Rect(
 				rect.x + usedWidth + ColumnSpace,
@@ -185,28 +155,37 @@ namespace MBSCore.Scriptable
 			EditorGUIUtility.PingObject(value);
 		}
 
-		private void RemoveElement()
+		private void RemoveElement(ReorderableList list)
 		{
-			if (_lastCreateIndex < 0)
+			int listCount = list.list.Count;
+			T removedObject = List.list[list.index] as T;
+			var newList = new List<T>(listCount - 1);
+			for (int i = 0; i < listCount; i++)
 			{
-				return;
+				if (i == list.index)
+				{
+					continue;
+				}
+				
+				newList.Add(list.list[i] as T);
 			}
 			
-			T removedObject = List.list[_lastChangedIndex] as T;
-			List.list.RemoveAt(_lastChangedIndex);
+			if(list.list is Array)
+			{
+				FieldInfo.SetValue(Target, newList.ToArray());
+			}
+			else
+			{
+				FieldInfo.SetValue(Target, newList);
+			}
+
 			if (removedObject == null)
 			{
 				return;
 			}
 
-			if (_editors.Remove(removedObject, out Editor editor))
-			{
-				Object.DestroyImmediate(editor);
-			}
-
 			Object.DestroyImmediate(removedObject, true);
 			AssetDatabase.SaveAssets();
-			_lastChangedIndex = -1;
 		}
 	}
 }
